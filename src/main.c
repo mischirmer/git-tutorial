6377636b9667e505f488590fd610482c2df7e98b
Michael Schirmer <m.schirmer@tum.de>
2022-10-20 11:23:49 +0200

    code for second coding challenge

--- src/main.c
+++ src/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include "espl_lib.h"
 
 int main(){
-	printf("Hello ESPL\n");
+	unsigned int input_number;
+
+	while(1){
+		printf("Please input a positive integer: ");
+		scanf("%d", &input_number);
+		printf("%s\n", num_to_words(input_number));
+
+		printf("\n------------------------------------\nInput \n\t0 to exit the program.\n\t1 to run another number.\n------------------------------------\n");
+		scanf("%d", &input_number);
+		printf("\n");
+		if (input_number == 0){
+			printf("Exiting...\n");
+			return 0;
+		}
+		
+	}
 	return 0;
 }
